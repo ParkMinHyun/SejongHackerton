4ec0ee9413063169e5e715fd037e521d0e3048f4
ParkMinHyun <algusl92@naver.com>
2017-06-27 07:20:23 +0900

    응용 SW Track 완성

--- TrackEngine/app/src/main/java/com/example/parkminhyun/trackengine/ApplicationSWActivity.java
+++ TrackEngine/app/src/main/java/com/example/parkminhyun/trackengine/ApplicationSWActivity.java
@@ -5,6 +5,7 @@ import android.os.Build;
 import android.os.Bundle;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.Window;
 import android.view.WindowManager;
@@ -59,7 +60,6 @@ public class ApplicationSWActivity extends AppCompatActivity {
         computerVisionSystemImageView = (ImageView)findViewById(R.id.computerVisionSystemImageView);
         VoiceProcessingSoftwareImageView = (ImageView)findViewById(R.id.VoiceProcessingSoftwareImageView);
 
-
         TroubleshootingAndPracticeTextView = (TextView) findViewById(R.id.TroubleshootingAndPracticeTextView);
         multiMediaProgrammingTextView = (TextView)findViewById(R.id.multiMediaProgrammingTextView);
         windowProgrammingTextView = (TextView)findViewById(R.id.windowProgrammingTextView);
@@ -70,46 +70,62 @@ public class ApplicationSWActivity extends AppCompatActivity {
         computerVisionSystemTextView = (TextView)findViewById(R.id.computerVisionSystemTextView);
         VoiceProcessingSoftwareTextView = (TextView)findViewById(R.id.VoiceProcessingSoftwareTextView);
 
+        rvSubject = (RecyclerView) findViewById(R.id.rv_subject);
+        LinearLayoutManager llm = new LinearLayoutManager(this);
+        rvSubject.setLayoutManager(llm);
+        rvSubject.setHasFixedSize(true);
+
         subjects = new ArrayList<>();
         userStudyData = new UserStudyData(this);
         List<String> userStudy = userStudyData.loadStudyDataList();
 
+        subjects.add(new Subject("[2-1]", "문제해결 및 실습:C++"));
+        subjects.add(new Subject("[2-1]", "멀티미디어 프로그래밍"));
+        subjects.add(new Subject("[2-2]", "윈도우즈 프로그래밍"));
+        subjects.add(new Subject("[3-1]", "모바일 프로그래밍"));
+        subjects.add(new Subject("[3-2]", "웹 프로그래밍"));
+        subjects.add(new Subject("[3-2]", "디지털 사운드"));
+        subjects.add(new Subject("[4-1]", "영상처리"));
+        subjects.add(new Subject("[4-2]", "컴퓨터 비전 시스템"));
+        subjects.add(new Subject("[4-2]", "음성처리 SW"));
         for(int i=0; i< userStudy.size(); i++) {
             switch (userStudy.get(i).toString()) {
                 case "문제해결 및 실습: C++":
                     multiMediaProgrammingImageView.setBackgroundColor(Color.rgb(52, 152, 219));
-                    TroubleshootingAndPracticeTextView.setTextColor(Color.BLACK); break;
-//                    subjects.remove(0); break;
+                    TroubleshootingAndPracticeTextView.setTextColor(Color.BLACK);
+                    subjects.remove(0); break;
                 case "멀티미디어 프로그래밍":
                     multiMediaProgrammingImageView.setBackgroundColor(Color.rgb(52, 152, 219));
-                    multiMediaProgrammingTextView.setTextColor(Color.BLACK); break;
-//                    subjects.remove(1); break;
+                    multiMediaProgrammingTextView.setTextColor(Color.BLACK);
+                    subjects.remove(1); break;
                 case "윈도우즈 프로그래밍":
                     windowProgrammingImageView.setBackgroundColor(Color.rgb(52, 152, 219));
-                    windowProgrammingTextView.setTextColor(Color.BLACK); break;
-//                    subjects.remove(2); break;
+                    windowProgrammingTextView.setTextColor(Color.BLACK);
+                    subjects.remove(2); break;
                 case "모바일 프로그래밍":
                     mobileProgrammingImageView.setBackgroundColor(Color.rgb(52, 152, 219));
-                    mobileProgrammingTextView.setTextColor(Color.BLACK); break;
-//                    subjects.remove(3); break;
+                    mobileProgrammingTextView.setTextColor(Color.BLACK);
+                    subjects.remove(3); break;
                 case "웹프로그래밍":
                     webServerProgrammingImageView.setBackgroundColor(Color.rgb(52, 152, 219));
-                    webServerProgrammingTextView.setTextColor(Color.BLACK); break;
-//                    subjects.remove(4); break;
+                    webServerProgrammingTextView.setTextColor(Color.BLACK);
+                    subjects.remove(4); break;
                 case "디지털 사운드":
                     digitalSoundImageView.setBackgroundColor(Color.rgb(52, 152, 219));
-                    digitalSoundTextView.setTextColor(Color.BLACK); break;
-//                    subjects.remove(5); break;
+                    digitalSoundTextView.setTextColor(Color.BLACK);
+                    subjects.remove(5); break;
                 case "영상처리":
                     imageProcessingImageView.setBackgroundColor(Color.rgb(52, 152, 219));
-                    imageProcessingTextView.setTextColor(Color.BLACK); break;
-//                    subjects.remove(6); break;
+                    imageProcessingTextView.setTextColor(Color.BLACK);
+                    subjects.remove(6); break;
                 case "컴퓨터비전 시스템":
                     computerVisionSystemImageView.setBackgroundColor(Color.rgb(52, 152, 219));
-                    VoiceProcessingSoftwareTextView.setTextColor(Color.BLACK); break;
-//                    subjects.remove(7); break;
+                    VoiceProcessingSoftwareTextView.setTextColor(Color.BLACK);
+                    subjects.remove(7); break;
             }
         }
+        adapter = new RVAdapterVirtualReality(this, subjects);
+        rvSubject.setAdapter(adapter);
     }
 
     public void makeTransParentStatusBar(){
