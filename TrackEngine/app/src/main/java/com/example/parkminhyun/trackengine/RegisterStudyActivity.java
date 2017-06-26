9d7239fb1334a8b0453c7ed16dcd382eb86e95b0
DESKTOP-3RFQDQR\ParkMinHyun <algusl92@naver.com>
2017-06-26 14:06:41 +0900

    교과목 등록 버튼 생성 및 Intent 적용

---
+++ TrackEngine/app/src/main/java/com/example/parkminhyun/trackengine/RegisterStudyActivity.java
@@ -0,0 +1,13 @@
+package com.example.parkminhyun.trackengine;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class RegisterStudyActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register_study);
+    }
+}
