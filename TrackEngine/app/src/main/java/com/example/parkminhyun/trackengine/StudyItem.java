724e36da4231844dba7d6df9f63a2140dc517772
ParkMinHyun <algusl92@naver.com>
2017-06-26 15:25:18 +0900

    교과목 등록 Expandable ListView 적용

---
+++ TrackEngine/app/src/main/java/com/example/parkminhyun/trackengine/StudyItem.java
@@ -0,0 +1,13 @@
+package com.example.parkminhyun.trackengine;
+
+public class StudyItem {
+
+    public String studyName;
+    boolean checked;
+
+    StudyItem(String studyName, boolean checked) {
+        this.studyName = studyName;
+        this.checked = checked;
+
+    }
+}
