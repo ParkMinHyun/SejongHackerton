78b8bea631842901687f9010b4146eab4dac02b8
ParkMinHyun <algusl92@naver.com>
2017-06-27 07:11:51 +0900

    AI Track 완성

--- TrackEngine/app/src/main/java/com/example/parkminhyun/trackengine/ArtificialIntelligenceActivity.java
+++ TrackEngine/app/src/main/java/com/example/parkminhyun/trackengine/ArtificialIntelligenceActivity.java
@@ -5,6 +5,7 @@ import android.os.Build;
 import android.os.Bundle;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.Window;
 import android.view.WindowManager;
@@ -49,6 +50,11 @@ public class ArtificialIntelligenceActivity extends AppCompatActivity {
         titleBarImage = (ImageView)findViewById(R.id.imageView2);
         titleBarImage.setImageResource(R.drawable.artificial_intelligence);
 
+        rvSubject = (RecyclerView) findViewById(R.id.rv_subject);
+        LinearLayoutManager llm = new LinearLayoutManager(this);
+        rvSubject.setLayoutManager(llm);
+        rvSubject.setHasFixedSize(true);
+
         multiMediaProgrammingImageView = (ImageView)findViewById(R.id.multiMediaProgrammingImageView);
         ArtificialIntelligenceImageView = (ImageView)findViewById(R.id.ArtificialIntelligenceImageView);
         dataBaseProgrammingImageView = (ImageView)findViewById(R.id.dataBaseProgrammingImageView);
@@ -73,37 +79,57 @@ public class ArtificialIntelligenceActivity extends AppCompatActivity {
         userStudyData = new UserStudyData(this);
         List<String> userStudy = userStudyData.loadStudyDataList();
 
+        subjects.add(new Subject("[2-1]", "멀티미디어 프로그래밍"));
+        subjects.add(new Subject("[3-2]", "인공지능"));
+        subjects.add(new Subject("[3-2]", "데이터베이스 프로그래밍"));
+        subjects.add(new Subject("[3-2]", "데이터 분석"));
+        subjects.add(new Subject("[4-1]", "기계학습"));
+        subjects.add(new Subject("[4-1]", "영상처리"));
+        subjects.add(new Subject("[4-2]", "패턴 인식"));
+        subjects.add(new Subject("[4-2]", "컴퓨터비전 시스템"));
+        subjects.add(new Subject("[4-2]", "지능형 시스템"));
         for(int i=0; i< userStudy.size(); i++){
             switch (userStudy.get(i).toString()){
                 case "멀티미디어 프로그래밍" :
                     multiMediaProgrammingImageView.setBackgroundColor(Color.rgb(52, 152, 219));
-                    multiMediaProgrammingTextView.setTextColor(Color.BLACK);break;
+                    multiMediaProgrammingTextView.setTextColor(Color.BLACK);
+                    subjects.remove(0); break;
                 case "인공지능" :
                     ArtificialIntelligenceImageView.setBackgroundColor(Color.rgb(52, 152, 219));
-                    ArtificialIntelligenceTextView.setTextColor(Color.BLACK);break;
+                    ArtificialIntelligenceTextView.setTextColor(Color.BLACK);
+                    subjects.remove(1); break;
                 case "데이터베이스 프로그래밍" :
                     dataBaseProgrammingImageView.setBackgroundColor(Color.rgb(52, 152, 219));
-                    dataBaseProgrammingTextView.setTextColor(Color.BLACK); break;
+                    dataBaseProgrammingTextView.setTextColor(Color.BLACK);
+                    subjects.remove(2); break;
                 case "데이터 분석" :
                     dataAnalysisImageView.setBackgroundColor(Color.rgb(52, 152, 219));
-                    dataAnalysisTextView.setTextColor(Color.BLACK); break;
+                    dataAnalysisTextView.setTextColor(Color.BLACK);
+                    subjects.remove(3); break;
                 case "기계학습" :
                     machineLearningImageView.setBackgroundColor(Color.rgb(52, 152, 219));
-                    machineLearningTextView.setTextColor(Color.BLACK);break;
+                    machineLearningTextView.setTextColor(Color.BLACK);
+                    subjects.remove(4); break;
                 case "영상처리" :
                     imageProcessingImageView.setBackgroundColor(Color.rgb(52, 152, 219));
-                    imageProcessingTextView.setTextColor(Color.BLACK); break;
+                    imageProcessingTextView.setTextColor(Color.BLACK);
+                    subjects.remove(5); break;
                 case "패턴 인식" :
                     patternRecognitionImageView.setBackgroundColor(Color.rgb(52, 152, 219));
-                    patternRecognitionTextView.setTextColor(Color.BLACK); break;
+                    patternRecognitionTextView.setTextColor(Color.BLACK);
+                    subjects.remove(6); break;
                 case "컴퓨터 비전 시스템" :
                     computerVisionSystemImageView.setBackgroundColor(Color.rgb(52, 152, 219));
-                    computerVisionSystemTextView.setTextColor(Color.BLACK); break;
+                    computerVisionSystemTextView.setTextColor(Color.BLACK);
+                    subjects.remove(7); break;
                 case "지능형 시스템" :
                     intelligentSystemImageView.setBackgroundColor(Color.rgb(52, 152, 219));
-                    intelligentSystemTextView.setTextColor(Color.BLACK); break;
+                    intelligentSystemTextView.setTextColor(Color.BLACK);
+                    subjects.remove(8); break;
             }
         }
+        adapter = new RVAdapterVirtualReality(this, subjects);
+        rvSubject.setAdapter(adapter);
     }
 
     public void makeTransParentStatusBar(){
